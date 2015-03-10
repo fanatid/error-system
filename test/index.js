c6542b917e34fe76fae57dd92ae88a6f038bbd8a
Kirill Fomichev <fanatid@ya.ru>
2015-03-10 16:08:35 +0300

    increase coverage

--- test/index.js
+++ test/index.js
@@ -1,9 +1,19 @@
-/* global describe, it */
+/* global describe, it, afterEach */
 
 var expect = require('chai').expect
 var errorSystem = require('../lib')
 var extend = errorSystem.extend
 var createError = errorSystem.createError
+
+function testCustomError (CError, PError, ceName, ceArgs, ceMessage) {
+  expect(CError).to.be.a('Function')
+  expect(PError[ceName]).to.equal(CError)
+  var CErrorConstructor = Function.prototype.bind.apply(CError, [null].concat(ceArgs))
+  var custom = new CErrorConstructor()
+  expect(custom).to.be.instanceof(PError)
+  expect(custom).to.be.instanceof(CError)
+  expect(custom.message).to.equal(ceMessage)
+}
 
 describe('extend', function () {
   it('invalid parent', function () {
@@ -36,22 +46,12 @@ describe('extend', function () {
       return 'arguments: ' + Array.prototype.join.call(arguments, ',')
     }
     var Custom2 = extend(Error, {name: 'Custom2', message: getMessage})
-    expect(Custom2).to.be.a('Function')
-    expect(Error.Custom2).to.equal(Custom2)
-    var custom2 = new Custom2('a', 'b')
-    expect(custom2).to.be.instanceof(Error)
-    expect(custom2).to.be.instanceof(Custom2)
-    expect(custom2.message).to.equal('arguments: a,b')
+    testCustomError(Custom2, Error, 'Custom2', ['a', 'b'], 'arguments: a,b')
   })
 
   it('spec.message a string', function () {
     var Custom3 = extend(Error, {name: 'Custom3', message: 'arguments: {0},{1},{2}'})
-    expect(Custom3).to.be.a('Function')
-    expect(Error.Custom3).to.equal(Custom3)
-    var custom3 = new Custom3('a', 'b')
-    expect(custom3).to.be.instanceof(Error)
-    expect(custom3).to.be.instanceof(Custom3)
-    expect(custom3.message).to.equal('arguments: a,b,{2}')
+    testCustomError(Custom3, Error, 'Custom3', ['a', 'b'], 'arguments: a,b,{2}')
   })
 
   it('recursive extend via errors', function () {
@@ -72,41 +72,54 @@ describe('extend', function () {
       ]
     }
 
-    function testCustomError (CustomError, ParentError, errorName, expectedMessage) {
-      expect(CustomError).to.be.a('Function')
-      expect(ParentError[errorName]).to.equal(CustomError)
-      var custom = new CustomError()
-      expect(custom).to.be.instanceof(ParentError)
-      expect(custom).to.be.instanceof(CustomError)
-      expect(custom.message).to.equal(expectedMessage)
-    }
-
     var Custom4 = extend(Error, spec)
-    testCustomError(Custom4, Error, 'Custom4', 'Custom4: 123')
-    testCustomError(Custom4.Custom5, Custom4, 'Custom5', 'Custom5: 234')
-    testCustomError(Custom4.Custom5.Custom6, Custom4.Custom5, 'Custom6', 'Custom6: 345')
+    testCustomError(Custom4, Error, 'Custom4', [], 'Custom4: 123')
+
+    var Custom5 = Custom4.Custom5
+    testCustomError(Custom5, Custom4, 'Custom5', [], 'Custom5: 234')
+
+    var Custom6 = Custom5.Custom6
+    testCustomError(Custom6, Custom5, 'Custom6', [], 'Custom6: 345')
   })
 })
 
 describe('createError', function () {
   it('with default parent (as Error)', function () {
     var Custom10 = createError('Custom10', 'Custom10: {0}')
-    expect(Custom10).to.be.a('Function')
-    expect(Error.Custom10).to.equal(Custom10)
-    var custom10 = new Custom10('h1')
-    expect(custom10).to.be.instanceof(Error)
-    expect(custom10).to.be.instanceof(Custom10)
-    expect(custom10.message).to.equal('Custom10: h1')
+    testCustomError(Custom10, Error, 'Custom10', ['h1'], 'Custom10: h1')
   })
 
   it('with custom parent', function () {
     function OtherError () {}
     var Custom11 = createError('Custom11', 'Custom11: {0}', OtherError)
-    expect(Custom11).to.be.a('Function')
-    expect(OtherError.Custom11).to.equal(Custom11)
-    var custom11 = new Custom11('h1')
-    expect(custom11).to.be.instanceof(OtherError)
-    expect(custom11).to.be.instanceof(Custom11)
-    expect(custom11.message).to.equal('Custom11: h1')
+    testCustomError(Custom11, OtherError, 'Custom11', ['h1'], 'Custom11: h1')
+  })
+})
+
+describe('stack property', function () {
+  var captureStackTrace = Error.captureStackTrace
+
+  afterEach(function () {
+    Error.captureStackTrace = captureStackTrace
+  })
+
+  it('use (new Error()).stack for stack', function () {
+    delete Error.captureStackTrace
+
+    var Custom21 = createError('Custom21', '{0}', Error)
+    var custom21 = new Custom21()
+    expect(custom21.stack.split('\n')[0]).to.equal('Error')
+    expect(custom21.stack.split('\n')[2]).to.match(/test\/index\.js/)
+  })
+
+  it('use captureStackTrace for stack', function () {
+    if (typeof Error.captureStackTrace === 'undefined') {
+      return
+    }
+
+    var Custom22 = createError('Custom22', '{0}', Error)
+    var custom22 = new Custom22()
+    expect(custom22.stack.split('\n')[0]).to.equal('ErrorCustom22: {0}')
+    expect(custom22.stack.split('\n')[1]).to.match(/test\/index\.js/)
   })
 })
